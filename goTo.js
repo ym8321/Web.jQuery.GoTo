ed81fdfab72ac463588ce3f3691f3b70e8da0485
ym8321 <a2451350@gmail.com>
2020-10-29 19:26:53 +0800

    滾動

--- goTo.js
+++ goTo.js
@@ -2,12 +2,42 @@
 $("*").each(function (index, element) {
     // 此元素被點選後執行
     $(this).click(function (e) { 
-    // 取得被點選元素的屬性 : data-gt-target
-    var target = $(this).attr("data-gt-target");   
-    var duration = $(this).attr("data-gt-duration");
-    var offset = $(this).attr("data-gt-offset");  
-    console.log("目標 : " + target) ;
-    console.log("時間 : " + target) ;
-    console.log("位移 : " + target) ;
+        // 取得被點選元素的屬性 : data-gt-target
+        var target = $(this).attr("data-gt-target");   
+        var duration = $(this).attr("data-gt-duration");
+        var offset = $(this).attr("data-gt-offset"); 
+      
+       //JS : 語法 : 判斷式
+       //if (條件) {程式區塊} 
+       //當條件成立 會執行城市區塊
+      
+       //如果 目標有資料 才會執行 {} 內的程式
+       //避免出現 undefine  (未定義 - 不存在的資料)
+       if (target) {
+           console.log("目標 : " + target) ;
+           console.log("時間 : " + duration) ;
+           console.log("位移 : " + offset) ;
+
+
+           //上方位置 = 目標區塊.位移().上方位置
+           var top = $(target).offset().top;
+           console.log("要前往元素的上方位置 : " + top);
+
+
+           // 網頁元素.動畫({ 上方捲動 : 指定元素 - 位移}，持續時間)
+           // parseInt() 將文字轉為數字
+           
+           $("html").stop().animate({
+               scrollTop: top - offset      
+            }, parseInt(duration));
+           
+        }
+    
     });
+});
+
+// 避免動畫與使用者滾輪衝突
+// html 在滾動滾輪時 停止 html 所有效果
+$("html").on("mousewheel",function () {
+    $("html").stop();
 });
